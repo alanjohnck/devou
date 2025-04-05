fb2b24bedb57165821cada66d7ead709b2e53afc
alanjohnck <alanjohnchacko.live@gmail.com>
2025-04-05 15:05:17 +0530

    logo issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -45,6 +45,11 @@ export default function RootLayout({ children }) {
           href="/favicon.ico"
           type="image/x-icon"
         />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<link rel="manifest" href="/site.webmanifest" />
           <meta property="og:title" content="Devou - Design Develop Deploy" />
         <meta property="og:description" content="We offer cutting-edge web development, design, and digital solutions." />
         <meta property="og:url" content="https://www.devou.in/" />
